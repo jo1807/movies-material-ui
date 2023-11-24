aaecf753c9d70e0464b02159860e6c40a658bad1
Joanne Notley <49209412+jo1807@users.noreply.github.com>
2023-11-24 20:55:21 +0000

    add type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,20 @@
 export interface IMovieData {
-    cost: number;
-    filmCompanyId: string;
-    id: string;
-    releaseYear: number;
-    reviews: number[];
-    title: string
-  }
-  
+  cost: number;
+  filmCompanyId: string;
+  id: string;
+  releaseYear: number;
+  reviews: number[];
+  title: string;
+}
+
 export interface IFilmCompaniesData {
-    id: string;
-    name: string;
-  }
+  id: string;
+  name: string;
+}
+
+export interface IMappedMovieData {
+  id: string;
+  averageReviewScore: string;
+  filmCompany: string | undefined;
+  title: string;
+}
